da0d9ab32f011c4fc1ed35632bb4418fbaadcf30
RanYeee <IMac@RanydeiMac.local>
2016-03-11 14:33:54 +0800

    commit

---
+++ OnlyNote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnlyNote
+//
+//  Created by IMac on 16/3/11.
+//  Copyright © 2016年 IMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OnlyNote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OnlyNote
+//
+//  Created by IMac on 16/3/11.
+//  Copyright © 2016年 IMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
